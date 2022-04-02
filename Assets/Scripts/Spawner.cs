50407fdd3ee1c95039e76394b5e1d4a0fe8e6563
BurningPride <56182492+mwesterham@users.noreply.github.com>
2022-04-01 23:23:35 -0700

    Reworked spawning mechanic to be from a single prefab

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> subjects;
+    public List<Transform> spawnLocations;
     public float[] timeBetweenRange = new float[2];
     public float timeTillNext = 1f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if(timeTillNext <= 0) {
-            int rand_index = Random.Range(0, subjects.Count);
-            Instantiate(subjects[rand_index], this.transform.position, this.transform.rotation);
+            Transform thisLocation = spawnLocations[Random.Range(0, spawnLocations.Count)];
+            Instantiate(subjects[Random.Range(0, subjects.Count)], thisLocation.position, thisLocation.rotation);
             timeTillNext = Random.Range(timeBetweenRange[0], timeBetweenRange[1]);
         }
         timeTillNext -= Time.deltaTime;
