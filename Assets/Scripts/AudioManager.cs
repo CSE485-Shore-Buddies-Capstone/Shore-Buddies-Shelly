fa900b17a39ea2e0993df7d7a50deaf143de4c27
Aira San Agustin <airadani25@gmail.com>
2022-04-02 00:41:46 -0700

    Imported fonts, edited setting ui panel, hook volume functions

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioSource backgroundMusic;
+    public Slider volumeSlider;
     // Start is called before the first frame update
     void Start()
     {
         playBackgroundMusic();
+        volumeSlider.onValueChanged.AddListener(setBackgroundVolume);
     }
 
     public void setBackgroundVolume(float volume) {
