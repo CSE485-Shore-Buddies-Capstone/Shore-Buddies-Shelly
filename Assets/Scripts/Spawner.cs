3f50e6f6b45dcfc10300654aa64b933c8edf5ed1
BurningPride <56182492+mwesterham@users.noreply.github.com>
2022-04-01 23:49:38 -0700

    Made the spawning to be relative to a camera space
    Randomizes the location based on where the camera is

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> subjects;
-    public List<Transform> spawnLocations;
     public float[] timeBetweenRange = new float[2];
     public float timeTillNext = 1f;
+    public Camera mainCamera;
+
+    private Vector3 pointTopLeft;
+    private Vector3 pointTopRight;
+
+    void Start() {
+        pointTopLeft = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
+        pointTopRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, mainCamera.nearClipPlane));
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(timeTillNext <= 0) {
-            Transform thisLocation = spawnLocations[Random.Range(0, spawnLocations.Count)];
-            Instantiate(subjects[Random.Range(0, subjects.Count)], thisLocation.position, thisLocation.rotation);
+            float xSpawn = Random.Range(pointTopLeft.x, pointTopRight.x);
+            float ratioAboveScreen = 5f/4f;
+            Vector3 thisLocation = new Vector3(xSpawn, -pointTopLeft.y * ratioAboveScreen, 0f);
+            Instantiate(subjects[Random.Range(0, subjects.Count)], thisLocation, new Quaternion());
             timeTillNext = Random.Range(timeBetweenRange[0], timeBetweenRange[1]);
         }
         timeTillNext -= Time.deltaTime;
