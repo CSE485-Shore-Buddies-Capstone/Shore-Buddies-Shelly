d9260b526c77944beddd557a020ad2c05df713ed
Aira San Agustin <airadani25@gmail.com>
2022-04-10 00:27:23 -0700

    Started Shelly animations and page transitions
    idle animation for shelly
    page animation fade out on SampleHome scene
    messed around with particle systems for a bubble effect transition

---
+++ Assets/Scripts/HomeUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeUI : MonoBehaviour
+{
+    [Header("UI")]
+    public Button playButton;
+
+    [Header("Page Transition")]
+    public Animator transition;
+    public float transitionTime;
+    //public GameObject bubbleEffect; //TODO: still working on it
+
+    void Start(){
+        playButton.onClick.AddListener(PlayGameWrap);
+    }
+
+    //wrapper to call the coroutined transition when play button is clicked
+    private void PlayGameWrap(){
+        StartCoroutine(PlayGame());
+    }
+
+    private IEnumerator PlayGame(){
+        // bubbleEffect.SetActive(true);
+        transition.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(transitionTime);
+        SceneLoader.Load("Menu");
+    }
+}
