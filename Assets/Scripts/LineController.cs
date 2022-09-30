0c7dfb4a2be41f7c0853f5d85b16b076e0f0de4d
BurningPride <56182492+mwesterham@users.noreply.github.com>
2022-09-30 15:14:23 -0700

    Fishing line now synced to button press
    - Small rework in fishing rod logic
    - Also linked main menu to fishing game

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineController : MonoBehaviour
+{
+    [Header("UI")]
+    public Button holdButton;
+    
+    [Header("Line Objects")]
+    public GameObject catcher;
+    public Transform origin;
+    public Transform destination;
+    public float maxCastDurationSeconds = 3f;
+
+    private LineRenderer line;
+    private float percentOfDistance = 0;
+    private float distance;
+    private bool casted, locked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.SetPosition(0, origin.position);
+        distance = Vector3.Distance(origin.position, destination.position);
+    }
+
+    void Update()
+    {
+        // hook will always be at the end of the line
+        catcher.transform.position = line.GetPosition(1);
+        
+        if (casted && !locked){
+            // cast
+            if(percentOfDistance < 1.0f){
+                percentOfDistance += Time.deltaTime / maxCastDurationSeconds;
+                setLinePosition(percentOfDistance);
+            }
+            else
+                casted = false;
+        }
+        else {
+            // reel
+            locked = true;
+            if(percentOfDistance > 0){
+                percentOfDistance -= Time.deltaTime / maxCastDurationSeconds;
+                setLinePosition(percentOfDistance);
+            }
+            else
+                locked = false;
+        }
+    }
+
+    public void CastReel() {
+        if(!locked)
+            casted = true;
+    }
+
+    private void setLinePosition(float percentageThrough) {
+        float percentedPoint = Mathf.Lerp(0, distance, percentageThrough);
+        Vector3 pointAlongLine = (percentedPoint * Vector3.Normalize(destination.position-origin.position ))+origin.position;
+        line.SetPosition(1, pointAlongLine);
+    }
+}
