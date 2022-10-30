4eb7de9c43162afaf81ea0d5beede62d60c2afc6
Aira Daniella San Agustin <55384817+ai-dani@users.noreply.github.com>
2022-10-30 00:23:37 -0700

    Functionality for saving sound settings

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,12 +6,40 @@ using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
     public AudioSource backgroundMusic;
+    private int unmuted; // 0 = muted, 1 = unmuted
+
+    [Header("Volume UI (Drag from hierarchy)")]
     public Slider volumeSlider;
+    public GameObject volumeOnButton;
+    public GameObject volumeOffButton;
+    
     // Start is called before the first frame update
     void Start()
     {
-        playBackgroundMusic();
         volumeSlider.onValueChanged.AddListener(setBackgroundVolume);
+        
+        // default values unless there are saved sound settings
+        float volume = 0.5f;
+        int unmuteState = 1; 
+
+        // load saved volume
+        if(PlayerPrefs.HasKey("SavedVolume")){
+            volume = PlayerPrefs.GetFloat("SavedVolume");
+            setBackgroundVolume(volume);
+        }
+
+        // load music status (mute/unmute)
+        if(PlayerPrefs.HasKey("SavedMuteUnmute")){
+            unmuteState = PlayerPrefs.GetInt("SavedMuteUnmute");
+            if(unmuteState == 1){
+                playBackgroundMusic();
+            }else{
+                pauseBackgroundMusic();
+            }
+        }
+
+        updateVolumeUI(volume, unmuteState);
+
     }
 
     public void setBackgroundVolume(float volume) {
@@ -19,10 +47,31 @@ public class AudioManager : MonoBehaviour
     }
 
     public void pauseBackgroundMusic() {
+        unmuted = 0;
         this.backgroundMusic.Pause();
     }
 
     public void playBackgroundMusic() {
+        unmuted = 1;
         this.backgroundMusic.Play();
+    }
+
+    public void saveSoundSettings(){
+        PlayerPrefs.SetFloat("SavedVolume", this.backgroundMusic.volume);
+        PlayerPrefs.SetInt("SavedMuteUnmute", unmuted);
+    }
+
+    public void updateVolumeUI(float volume, int unmuteState){
+        if(volumeSlider == null || volumeOnButton == null || volumeOffButton == null) return;
+
+        volumeSlider.value = volume;
+
+        if(unmuteState==1){
+            volumeOnButton.SetActive(true);
+            volumeOffButton.SetActive(false);
+        }else{
+            volumeOnButton.SetActive(false);
+            volumeOffButton.SetActive(true);
+        }
     }
 }
