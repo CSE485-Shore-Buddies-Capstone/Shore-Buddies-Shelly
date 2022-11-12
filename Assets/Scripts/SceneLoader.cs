a0973d52a22a90e0c5a11b6b291830024406be24
Aira Daniella San Agustin <55384817+ai-dani@users.noreply.github.com>
2022-11-12 05:32:45 -0700

    Add horizontal/vertical screen options, add settings to home
    **Need to fix leaderboard button

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -13,6 +13,7 @@ public class SceneLoader: MonoBehaviour
 
     void Start(){
         canvas.SetActive(false);
+        SetHorizontal(); // by default
     }
 
     //will temporarily take in string scene (move onto enum later)
@@ -27,4 +28,31 @@ public class SceneLoader: MonoBehaviour
         yield return new WaitForSecondsRealtime(transitionTime);
         SceneManager.LoadScene(scene);
     }
+
+    public void SetVertical(){
+        int val_width = Mathf.RoundToInt(Screen.width * (16f/9f));
+        int val_height = Mathf.RoundToInt(Screen.height * (9f / 16f));
+        Screen.SetResolution(Screen.width, val_width,FullScreenMode.MaximizedWindow,1);
+        Screen.SetResolution(val_height, Screen.height, FullScreenMode.MaximizedWindow,1);
+    }
+
+    public void SetHorizontal(){
+        int val_width = Mathf.RoundToInt(Screen.width * (9f/16f));
+        int val_height = Mathf.RoundToInt(Screen.height * (16f / 9f));
+        Screen.SetResolution(Screen.width, val_width,FullScreenMode.MaximizedWindow,1);
+        Screen.SetResolution(val_height, Screen.height, FullScreenMode.MaximizedWindow,1);
+    }
+
+    public void ExitApplication(){
+        Application.Quit();
+    }
+    // for application exit
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
 }
