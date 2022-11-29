c553c07fa4986b703078521aa906ae4cead500dc
BurningPride <56182492+mwesterham@users.noreply.github.com>
2022-11-28 22:37:38 -0700

    Added better UI home

--- Assets/Scripts/HomeScreen/UIScript.cs
+++ Assets/Scripts/HomeScreen/UIScript.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class UIScript : MonoBehaviour
 {
     public GameObject desktopLeaderboard, mobileLeaderboard;
+    public GameObject desktopSettings, mobileSettings;
 
     public void setLeaderBoardActive(bool active) {
         if (SystemInfo.deviceType == DeviceType.Handheld)
             mobileLeaderboard.SetActive(active);
         else
             desktopLeaderboard.SetActive(active);
+    }
+
+    public void setSettingsActive(bool active) {
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+            mobileSettings.SetActive(active);
+        else
+            desktopSettings.SetActive(active);
     }
 }
